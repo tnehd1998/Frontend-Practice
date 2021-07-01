570a672adb292e493a47cb7100e07c7da4fcf5ab
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-01 10:14:06 +0900

    Carrot Game Refactoring: Sound

--- Carrot Mini Game/src/field.js
+++ Carrot Mini Game/src/field.js
@@ -1,6 +1,6 @@
 "use strict";
+import * as sound from "./sound.js";
 
-const carrotSound = new Audio("./sound/carrot_pull.mp3");
 const CARROT_SIZE = 80;
 
 export default class Field {
@@ -45,7 +45,7 @@ export default class Field {
     const target = event.target;
     if (target.matches(".carrot")) {
       target.remove();
-      playSound(carrotSound);
+      sound.playCarrot();
       this.onItemClick && this.onItemClick("carrot");
     } else if (target.matches(".bug")) {
       this.onItemClick && this.onItemClick("bug");
@@ -55,9 +55,4 @@ export default class Field {
 
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
-}
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
 }
