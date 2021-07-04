ec28cfaca1e8eb6b6e87a13c34ce233c0bdf32ca
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-04 23:38:31 +0900

    React: Habit-Tracker Reset

--- React-Basic/Habit-Tracker/src/components/habits.jsx
+++ React-Basic/Habit-Tracker/src/components/habits.jsx
@@ -3,37 +3,24 @@ import Habit from "./habit";
 import HabitAddForm from "./habitAddForm";
 
 class Habits extends Component {
-  handleIncrement = (habit) => {
-    this.props.onIncrement(habit);
-  };
-
-  handleDecrement = (habit) => {
-    this.props.onDecrement(habit);
-  };
-
-  handleDelete = (habit) => {
-    this.props.onDelete(habit);
-  };
-
-  handleAdd = (name) => {
-    this.props.onAdd(name);
-  };
-
   render() {
     return (
       <>
-        <HabitAddForm onAdd={this.handleAdd} />
+        <HabitAddForm onAdd={this.props.onAdd} />
         <ul>
           {this.props.habits.map((habit) => (
             <Habit
               key={habit.id}
               habit={habit}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-              onDelete={this.handleDelete}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
             />
           ))}
         </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
       </>
     );
   }
