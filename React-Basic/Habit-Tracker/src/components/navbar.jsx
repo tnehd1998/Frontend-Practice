b2b36bec609f00edbe9b1e8e7891ebdaefc57cac
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-06 18:28:28 +0900

    React: Habit-Tracker Navbar Hook

--- React-Basic/Habit-Tracker/src/components/navbar.jsx
+++ React-Basic/Habit-Tracker/src/components/navbar.jsx
@@ -1,15 +1,13 @@
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 
-class Navbar extends PureComponent {
-  render() {
-    return (
-      <header className="navbar">
-        <i className="navbar-logo fas fa-leaf"></i>
-        <span>Habit Tracker</span>
-        <span className="navbar-count">{this.props.totalCount}</span>
-      </header>
-    );
-  }
-}
+const Navbar = memo((props) => {
+  return (
+    <header className="navbar">
+      <i className="navbar-logo fas fa-leaf"></i>
+      <span>Habit Tracker</span>
+      <span className="navbar-count">{props.totalCount}</span>
+    </header>
+  );
+});
 
 export default Navbar;
