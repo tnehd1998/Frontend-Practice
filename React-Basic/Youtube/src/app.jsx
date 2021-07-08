aea87f75ba46d6f7ee97424c253201133cf4088b
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-08 20:30:44 +0900

    React: Youtube Clone Search Function

--- React-Basic/Youtube/src/app.jsx
+++ React-Basic/Youtube/src/app.jsx
@@ -5,6 +5,23 @@ import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyCROyCIRNZU-ro57TqRxodwkFOns7BIhK8&part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCROyCIRNZU-ro57TqRxodwkFOns7BIhK8`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -22,7 +39,7 @@ function App() {
   }, []);
   return (
     <div className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
