dc8b32e18add6b4474dbd9ccc6dde5c737509fba
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-08 11:20:46 +0900

    React: Youtube Clone Popular Videos
    Showing the title of Popular Videos from Youtube

--- React-Basic/Youtube/src/app.jsx
+++ React-Basic/Youtube/src/app.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyCROyCIRNZU-ro57TqRxodwkFOns7BIhK8&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCROyCIRNZU-ro57TqRxodwkFOns7BIhK8",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, [videos]);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
