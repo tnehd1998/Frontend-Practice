e50b88200f45d8a5ca1043a9ba4a426dad8b312e
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-11 17:19:32 +0900

    project set up & add firebase

---
+++ React-Basic/card-maker/src/service/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DB_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
