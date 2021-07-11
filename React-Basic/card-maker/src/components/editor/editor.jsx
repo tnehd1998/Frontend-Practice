a6a6de4d20beabd4af4ebe4d478996805e10e43b
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-11 22:42:34 +0900

    React: Card Maker (Add Editor and Preview Screen)

---
+++ React-Basic/card-maker/src/components/editor/editor.jsx
@@ -0,0 +1,10 @@
+import styles from "./editor.module.css";
+import React from "react";
+
+const Editor = (props) => (
+  <section className={styles.editor}>
+    <h1 className={styles.title}>Card Maker</h1>
+  </section>
+);
+
+export default Editor;
---
+++ React-Basic/card-maker/src/components/preview/preview.jsx
@@ -0,0 +1,10 @@
+import styles from "./preview.module.css";
+import React from "react";
+
+const Preview = (props) => (
+  <section className={styles.preview}>
+    <h1 className={styles.title}>Card Preview</h1>
+  </section>
+);
+
+export default Preview;
