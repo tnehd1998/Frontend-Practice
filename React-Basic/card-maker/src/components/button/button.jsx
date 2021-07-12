7833bd393f3663300d19e80ad7692bfb8ba45e1c
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-12 16:53:56 +0900

    React: Card Maker (Improve Performance)

--- React-Basic/card-maker/src/components/button/button.jsx
+++ React-Basic/card-maker/src/components/button/button.jsx
@@ -1,10 +1,10 @@
 import styles from "./button.module.css";
-import React from "react";
+import React, { memo } from "react";
 
-const Button = ({ name, onClick }) => (
+const Button = memo(({ name, onClick }) => (
   <button className={styles.button} onClick={onClick}>
     {name}
   </button>
-);
+));
 
 export default Button;
--- React-Basic/card-maker/src/components/maker/maker.jsx
+++ React-Basic/card-maker/src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./maker.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
@@ -12,9 +12,9 @@ const Maker = ({ FileInput, authService, cardRepository }) => {
   const [cards, setCards] = useState({});
   const [userId, setUserId] = useState(historyState && historyState.id);
 
-  const onLogout = () => {
+  const onLogout = useCallback(() => {
     authService.logout();
-  };
+  }, [authService]);
 
   useEffect(() => {
     if (!userId) {
