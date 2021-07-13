033eb3a925a4d3f77135b7fb48d4fcb71c2d93f0
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-13 18:40:12 +0900

    TypeScript: Discriminated Union

---
+++ Typescript/1-Types/1-6-discriminated.ts
@@ -0,0 +1,37 @@
+{
+  // Ex) function: login -> success, fail
+  type SuccessState = {
+    result: "success";
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    result: "fail";
+    reason: string;
+  };
+
+  type LoginState = SuccessState | FailState;
+
+  function login(): LoginState {
+    return {
+      result: "success",
+      response: {
+        body: "Login",
+      },
+    };
+  }
+
+  // printLoginState(state: LoginState)
+  // success -> 🎉 body
+  // fail -> 😭 reason
+
+  function printLoginState(state: LoginState) {
+    if (state.result === "success") {
+      console.log(`🎉 ${state.response.body}`);
+    } else {
+      console.log(`😭 ${state.reason}`);
+    }
+  }
+}
