55f14b98a2414d981525ab080e7740eb238c9232
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-13 17:54:47 +0900

    TypeScript: Array

---
+++ Typescript/1-Types/1-3-array.ts
@@ -0,0 +1,15 @@
+{
+  // Array
+  const fruits: string[] = ["🍎", "🍊"];
+  const scores: Array<number> = [1, 3, 4];
+  function printArray(fruits: readonly string[]) {}
+
+  // Tuple
+  // 굳이 사용하지 않음
+  // interface, type alias, class로 대체 가능
+  let student: [string, number];
+  student = ["name", 123];
+  student[0]; // name
+  student[1]; // 123
+  const [name, age] = student;
+}
