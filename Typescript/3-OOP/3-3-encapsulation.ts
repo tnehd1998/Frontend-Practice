3d393eb230a2614ccfa8980c71a90b4a2d26b85d
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-14 16:19:40 +0900

    TypeScript: Chapter3 (Encapsulation)

---
+++ Typescript/3-OOP/3-3-encapsulation.ts
@@ -0,0 +1,43 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  // public : 외부에서 접근 가능
+  // private : 외부에서 접근 불가능
+  // protected : 상속한 자식 클래스만 접근 가능
+  class CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("Value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = CoffeeMaker.makeMachine(32);
+  maker.fillCoffeeBeans(32);
+}
