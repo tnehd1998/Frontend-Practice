56185b4376bfe08324dbdc488bebb5be590f3ea3
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-16 14:43:10 +0900

    TypeScript: Index

---
+++ Typescript/9-Advanced/9-2-index.ts
@@ -0,0 +1,30 @@
+{
+  const obj = {
+    name: "billy",
+  };
+  obj.name; // billy
+  obj["name"]; // billy
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: "male" | "female";
+  };
+
+  type Name = Animal["name"]; // string
+  const text: Name = "hello";
+
+  type Gender = Animal["gender"]; // 'male' | 'female'
+
+  type Keys = keyof Animal; // 'name' | 'age' | 'gender'
+  const key: Keys = "gender";
+
+  type Person = {
+    name: string;
+    gender: Animal["gender"];
+  };
+  const person: Person = {
+    name: "billy",
+    gender: "male",
+  };
+}
