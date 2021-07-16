52551e279653410c87ba861a619045a15dbb51af
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-07-16 14:58:04 +0900

    TypeScript: Map

---
+++ Typescript/9-Advanced/9-3-map.ts
@@ -0,0 +1,60 @@
+{
+  type Video = {
+    title: string;
+    author: string;
+  };
+
+  type Optional<T> = {
+    [P in keyof T]?: T[P]; // for...in
+  };
+
+  type ReadOnly<T> = {
+    readonly [P in keyof T]: T[P];
+  };
+  type VideoOptional = Optional<Video>;
+
+  type Animal = {
+    name: string;
+    age: number;
+  };
+
+  const animal: Optional<Animal> = {
+    name: "dog",
+  };
+  animal.name = "billy";
+
+  const video: ReadOnly<Video> = {
+    title: "hi",
+    author: "billy",
+  };
+
+  // type VideoOptional = {
+  //   title?: string;
+  //   author?: string;
+  //   description?: string;
+  // };
+
+  // type VideoReadOnly = {
+  //   readonly title: string;
+  //   readonly author: string;
+  //   readonly description: string;
+  // };
+
+  type Nullable<T> = {
+    [P in keyof T]: T[P] | null;
+  };
+
+  const obj2: Nullable<Video> = {
+    title: null,
+    author: null,
+  };
+
+  type Proxy<T> = {
+    get(): T;
+    set(value: T): void;
+  };
+
+  type Proxify<T> = {
+    [P in keyof T]: Proxy<T[P]>;
+  };
+}
