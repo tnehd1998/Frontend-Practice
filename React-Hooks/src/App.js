4c15cde8eb7d6d933c6e435cd147f25f93a2fb30
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-04 16:25:28 +0900

    React: React-Hooks (useTitle)

--- React-Hooks/src/App.js
+++ React-Hooks/src/App.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
 function App() {
-  const sayHello = () => console.log("hello");
-  const [number, setNumber] = useState(0);
-  const [aNumber, setANumber] = useState(0);
-  useEffect(sayHello, []);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 }
