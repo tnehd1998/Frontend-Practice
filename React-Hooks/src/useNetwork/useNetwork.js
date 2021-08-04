0ceab471a5f79d627ab4d9e1c621cbd5424ce0f5
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-04 18:53:02 +0900

    React: React-Hooks (useFadeIn & useNetwork)

--- React-Hooks/src/useNetwork/useNetwork.js
+++ React-Hooks/src/useNetwork/useNetwork.js
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-
-const useNetwork = (onChange) => {
+export const useNetwork = (onChange) => {
   const [status, setStatus] = useState(navigator.onLine);
   const handleChange = () => {
     if (typeof onChange === "function") {
@@ -19,17 +16,3 @@ const useNetwork = (onChange) => {
   }, []);
   return status;
 };
-
-function App() {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "We just went online" : "We are offline");
-  };
-  const online = useNetwork(handleNetworkChange);
-  return (
-    <div className="App">
-      <h1>{online ? "Online" : "Offline"}</h1>
-    </div>
-  );
-}
-
-export default App;
