5bd73604e5fd82f56d445491ed4ce563022338ff
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-04 13:53:26 +0900

    React: React-Hooks (UseState)

---
+++ React-Hooks/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+function App() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
