31c6f435f1c0d2eeae21a2e27293f7e1ced83176
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-04 14:15:59 +0900

    React: React-Hooks (useInput)

--- React-Hooks/src/App.js
+++ React-Hooks/src/App.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import "./App.css";
 
-function App() {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
 
+function App() {
+  const maxLength = (value) => !value.includes("@");
+  const name = useInput("Mr. ", maxLength);
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
