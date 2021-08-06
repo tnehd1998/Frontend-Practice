3bab9e0accf140c6ed1cdd4d5ffe2f990e5b154d
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-06 19:19:44 +0900

    GraphQL: Movie-Practice(Server, Query, Schema, Mutation)

--- GraphQL/graphql/resolvers.js
+++ GraphQL/graphql/resolvers.js
@@ -1,6 +1,13 @@
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
+
 const resolvers = {
   Query: {
-    name: () => "billy",
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
