8f7309fe789bab529428f781ab24113a29caf478
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-07 17:04:27 +0900

    GraphQL: Apollo (Local State)

--- GraphQL/Apollo-Movie/src/components/Movie.js
+++ GraphQL/Apollo-Movie/src/components/Movie.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
+
+const LIKE_MOVIE = gql`
+  mutation toggleLikeMovie($id: Int!, $isLiked: Boolean!) {
+    toggleLikeMovie(id: $id, isLiked: $isLiked) @client
+  }
+`;
 
 const Container = styled.div`
   height: 380px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
   border-radius: 7px;
 `;
 
@@ -18,10 +25,16 @@ const Poster = styled.div`
   background-position: center center;
 `;
 
-export default ({ id, bg }) => (
-  <Container>
-    <Link to={`/${id}`}>
-      <Poster bg={bg} />
-    </Link>
-  </Container>
-);
+export default ({ id, bg, isLiked }) => {
+  const [toggleMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id), isLiked },
+  });
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={toggleMovie}>{isLiked ? "Unlike" : "Like"}</button>
+    </Container>
+  );
+};
