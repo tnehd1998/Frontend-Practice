593608b1bbdcceb539afcb488365062588c235c7
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-10 13:48:32 +0900

    React: Netflix-Clone (Container)

--- React/Netflix-Clone/src/api.js
+++ React/Netflix-Clone/src/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
   },
 });
 
-export const tvApi = {
+export const moviesApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
   popular: () => api.get("movie/popular"),
@@ -26,7 +26,7 @@ export const tvApi = {
     }),
 };
 
-export const moviesApi = {
+export const tvApi = {
   topRated: () => api.get("tv/top_rated"),
   popular: () => api.get("tv/popular"),
   airingToday: () => api.get("tv/airing_today"),
--- React/Netflix-Clone/src/components/Router.js
+++ React/Netflix-Clone/src/components/Router.js
@@ -9,6 +9,7 @@ import Home from "../routes/Home";
 import TV from "../routes/TV";
 import Header from "../components/Header";
 import Search from "../routes/Search";
+import Detail from "../routes/Detail";
 
 export default () => (
   <Router>
@@ -18,6 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
