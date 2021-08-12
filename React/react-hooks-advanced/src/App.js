2b65e533fadb7f4cdbe20b3779a8b970acc20245
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-12 18:33:20 +0900

    React: React-Hooks (Simple ToDo List)

--- React/react-hooks-advanced/src/App.js
+++ React/react-hooks-advanced/src/App.js
@@ -1,26 +1,69 @@
-import React, { useReducer } from "react";
-
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
-    default:
-      throw new Error();
-  }
-};
+import React, { useReducer, useState } from "react";
+import reducer, { initialState, ADD, DEL, COM, UNCOM } from "./reducer";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>Minus</button>
+      <h1>Add to do</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
+      <ul>
+        {state.toDos.length !== 0 && (
+          <>
+            <h2>To Dos</h2>
+            {state.toDos.map((toDo) => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                  ❌
+                </span>
+                <span onClick={() => dispatch({ type: COM, payload: toDo.id })}>
+                  ✅
+                </span>
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
+      <ul>
+        {state.completed.length !== 0 && (
+          <>
+            <h2>Completed</h2>
+            {state.completed.map((toDo) => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                  ❌
+                </span>
+                <span
+                  onClick={() => dispatch({ type: UNCOM, payload: toDo.id })}
+                >
+                  🤮
+                </span>
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
     </>
   );
 };
---
+++ React/react-hooks-advanced/src/reducer.js
@@ -0,0 +1,46 @@
+import { v4 as uuid } from "uuid";
+
+export const initialState = {
+  toDos: [],
+  completed: [],
+};
+
+export const ADD = "add";
+export const DEL = "del";
+export const COM = "complete";
+export const UNCOM = "uncomplete";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      return {
+        ...state,
+        toDos: [...state.toDos, { text: action.payload, id: uuid() }],
+      };
+    case DEL:
+      return {
+        ...state,
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+      };
+    case COM:
+      const target = state.toDos.find((toDo) => toDo.id === action.payload);
+      return {
+        ...state,
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+        completed: [...state.completed, { ...target }],
+      };
+    case UNCOM:
+      const aTarget = state.completed.find(
+        (toDo) => toDo.id === action.payload
+      );
+      return {
+        ...state,
+        completed: state.completed.filter((toDo) => toDo.id !== action.payload),
+        toDos: [...state.toDos, { ...aTarget }],
+      };
+    default:
+      throw new Error();
+  }
+};
+
+export default reducer;
