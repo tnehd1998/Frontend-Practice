67e62704b9f01ca345a60244053cb6a945a987c6
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-13 15:26:05 +0900

    React: Twitter-Clone/Firebase (Setup)

---
+++ React/Twitter-Clone/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEeumo-PlnKH23cszmOjF4BVg49k6xx-U",
+  authDomain: "twitter-clone-612e2.firebaseapp.com",
+  projectId: "twitter-clone-612e2",
+  storageBucket: "twitter-clone-612e2.appspot.com",
+  messagingSenderId: "1097218398539",
+  appId: "1:1097218398539:web:d6e205e222fb896489f0ce",
+};
+
+export default firebase.initializeApp(firebaseConfig);
