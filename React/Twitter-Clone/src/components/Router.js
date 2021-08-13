84ce29815390ab97d80189e4fe513803ad4fc945
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-13 19:40:21 +0900

    React: Twitter-Clone/Firebase (Authentication)

--- React/Twitter-Clone/src/components/Router.js
+++ React/Twitter-Clone/src/components/Router.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Profile from "routes/Profile";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
