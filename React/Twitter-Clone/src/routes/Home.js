a70cda9f74ac8a18ea212cdfdbdcd996cb93d5c1
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-08-14 11:56:59 +0900

    React: Twitter-Clone/Firebase (Tweeting CRUD)

--- React/Twitter-Clone/src/routes/Home.js
+++ React/Twitter-Clone/src/routes/Home.js
@@ -1,7 +1,57 @@
-import React from "react";
+import Tweet from "components/Tweet";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
-  return <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  useEffect(() => {
+    dbService.collection("tweets").onSnapshot((snapshot) => {
+      const tweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTweets(tweetArray);
+    });
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("tweets").add({
+      text: tweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+      <div>
+        {tweets.map((tweet) => (
+          <Tweet
+            key={tweet.id}
+            tweetObj={tweet}
+            isOwner={tweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
