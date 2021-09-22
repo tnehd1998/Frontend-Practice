d13326fc8f37d142b1413f137b8cd2ba6c0ed56f
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-09-22 19:55:10 +0900

    The Complete Web Developer (SmartBrain:Refactoring)

--- Udemy/The Complete Web Developer/SmartBrain/facerecognition-api/server.js
+++ Udemy/The Complete Web Developer/SmartBrain/facerecognition-api/server.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const knex = require("knex");
 const bcrypt = require("bcrypt-nodejs");
 
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
 const db = knex({
   client: "pg",
   connection: {
@@ -18,86 +23,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send(database.users);
-});
-
-app.post("/signin", (req, res) => {
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch(() => res.status(400).json("Unable to get user"));
-      } else {
-        res.status(400).json("Wrong credentials");
-      }
-    })
-    .catch(() => res.status(400).json("Wrong credentials"));
-});
-
-app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash,
-        email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(() => res.status(400).json("Unable to register"));
-});
-
-app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not Found");
-      }
-    })
-    .catch(() => res.status(400).json("Error getting user"));
-});
-
-app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => {
-      res.json(entries[0]);
-    })
-    .catch(() => res.status(400).json("Unable to get entries"));
-});
+app.get("/", (req, res) => res.send(database.users));
+app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
+app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db));
+app.put("/image", (req, res) => image.handleImage(req, res, db));
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
--- Udemy/The Complete Web Developer/SmartBrain/facerecognition/src/App.js
+++ Udemy/The Complete Web Developer/SmartBrain/facerecognition/src/App.js
@@ -26,23 +26,24 @@ const particlesOptions = {
   },
 };
 
+const initialState = {
+  input: "",
+  imageUrl: "",
+  box: {},
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  },
+};
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: "",
-      imageUrl: "",
-      box: {},
-      route: "signin",
-      isSignedIn: false,
-      user: {
-        id: "",
-        name: "",
-        email: "",
-        entries: 0,
-        joined: "",
-      },
-    };
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -97,7 +98,8 @@ class App extends Component {
             .then((response) => response.json())
             .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(console.log);
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
@@ -106,7 +108,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === "signout") {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     } else if (route === "home") {
       this.setState({ isSignedIn: true });
     }
