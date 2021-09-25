e4f3210d909a85724505869b09e8eb0e5281a6b3
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-09-25 15:31:14 +0900

    Complete React Developer (Master Project: React Router and Routing)

--- Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/App.js
+++ Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
