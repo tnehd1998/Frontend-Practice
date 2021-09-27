d7b5704c89e0934c57c7844a458050baee26f863
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-09-27 18:13:59 +0900

    Complete React Developer (Master Project: Stripe Payments Part 1)

---
+++ Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/components/StripeButton/StripeButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JeFM8FvemUwaZKH5m7w154P2uSHf0R6ruobqSkmjgzVD65RMmw2meAL75rLgaLYcNKpIhIYTwQO2etzDRIY7b5z00eJH8umdX";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Success");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
--- Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/pages/CheckoutPage/CheckoutPage.jsx
+++ Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/pages/CheckoutPage/CheckoutPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
+import StripeButton from "../../components/StripeButton/StripeButton";
 import {
   selectCartItems,
   selectCartTotal,
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 04/27 - CVV: 123
+      </div>
+      <StripeButton price={total} />
     </div>
   );
 };
