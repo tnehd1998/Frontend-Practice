6b75cb50b30284e76c8d4b8d1d8244ea9f9c3c52
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-09-27 16:19:58 +0900

    Complete React Developer (Master Project: Redux 2)

---
+++ Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import CollectionPreview from "../CollectionPreview/CollectionPreview";
+import { selectCollections } from "../../redux/shop/shop-selector";
+
+import "./CollectionsOverview.scss";
+
+const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/pages/ShopPage/ShopPage.jsx
+++ Udemy/Complete React Developer/E-Commerce/crwn-clothing/src/pages/ShopPage/ShopPage.jsx
@@ -1,24 +1,12 @@
-import React, { Component } from "react";
-import CollectionPreview from "../../components/CollectionPreview/CollectionPreview.jsx";
-import ShopData from "./ShopData.js";
+import React from "react";
+import CollectionsOverview from "../../components/CollectionsOverview/CollectionsOverview";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: ShopData,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
