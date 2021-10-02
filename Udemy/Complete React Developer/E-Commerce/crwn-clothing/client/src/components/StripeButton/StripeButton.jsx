d48b5dbce7b4e2f537e08891d1e5ac8744654dbd
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-10-02 14:11:29 +0900

    Complete React Developer (Master Project: Stripe Payments Part 2 - Backend)

--- Udemy/Complete React Developer/E-Commerce/crwn-clothing/client/src/components/StripeButton/StripeButton.jsx
+++ Udemy/Complete React Developer/E-Commerce/crwn-clothing/client/src/components/StripeButton/StripeButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeButton = ({ price }) => {
     "pk_test_51JeFM8FvemUwaZKH5m7w154P2uSHf0R6ruobqSkmjgzVD65RMmw2meAL75rLgaLYcNKpIhIYTwQO2etzDRIY7b5z00eJH8umdX";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Success");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please sure you use the provided credit card!"
+        );
+      });
   };
 
   return (
