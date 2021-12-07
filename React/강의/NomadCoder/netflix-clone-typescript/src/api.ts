1d590659fd17ba77ec6995e38ef23877feae512c
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-12-07 23:18:27 +0900

    React: ReactJS 마스터클래스 (Netflix Clone : Home Screen + Slider)

---
+++ React/강의/NomadCoder/netflix-clone-typescript/src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = "5ac473a75aeab09b4ad49ccc12f29f83";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  pages: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
