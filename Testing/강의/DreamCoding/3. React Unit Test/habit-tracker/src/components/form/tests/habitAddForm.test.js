f9f77171c6802879bca9850762aecc884a250d25
Billy <tnehd1998@gmail.com>
2021-12-15 17:25:30 +0900

    Testing : TDD와 리액트 테스트 (컴포넌트 테스트)

---
+++ Testing/강의/DreamCoding/3. React Unit Test/habit-tracker/src/components/form/tests/habitAddForm.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import renderer from "react-test-renderer";
+import HabitAddForm from "../habitAddForm";
+
+describe("HabitAddForm", () => {
+  it("renders", () => {
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  describe("Form Submit", () => {
+    let onAdd;
+    let input;
+    let button;
+
+    beforeEach(() => {
+      onAdd = jest.fn();
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText("Habit");
+      button = screen.getByText("Add");
+    });
+
+    it("calls onAdd when button is clicked and valid habit is entered", () => {
+      userEvent.type(input, "New Habit");
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledWith("New Habit");
+    });
+
+    it("does not call onAdd when the habit is empty", () => {
+      userEvent.type(input, "");
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledTimes(0);
+    });
+  });
+});
--- Testing/강의/DreamCoding/3. React Unit Test/habit-tracker/src/components/navbar/navbar.jsx
+++ Testing/강의/DreamCoding/3. React Unit Test/habit-tracker/src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 
 class Navbar extends PureComponent {
   render() {
@@ -6,7 +6,9 @@ class Navbar extends PureComponent {
       <nav className="navbar">
         <i className="navbar-logo fas fa-leaf"></i>
         <span>Habit Tracker</span>
-        <span className="navbar-count">{this.props.totalCount}</span>
+        <span className="navbar-count" data-testid="total-count">
+          {this.props.totalCount}
+        </span>
       </nav>
     );
   }
