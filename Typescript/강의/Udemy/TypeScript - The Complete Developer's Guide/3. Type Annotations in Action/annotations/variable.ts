1fe4490ef81b4cc5481b6d9f318a41ef6512abc8
Billy <tnehd1998@gmail.com>
2021-12-17 16:53:58 +0900

    TypeScript: The Complete Developer's Guide (Type Annotations in Action)

---
+++ Typescript/강의/Udemy/TypeScript - The Complete Developer's Guide/3. Type Annotations in Action/annotations/variable.ts
@@ -0,0 +1,60 @@
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// Type Inference
+// 변수나 상수에 초기화 작업을 하면
+// 타입을 지정하지 않아도 타입스크립트가 알아서 타입을 지정해줌
+let apples = 5;
+
+// When to use annotations
+// 1) Functions that returns the "any" type
+const json = '{"x":10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y:20}
+
+// 2) When we declare a variable on one line and
+// initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
