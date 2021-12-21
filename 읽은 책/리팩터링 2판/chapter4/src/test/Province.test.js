c57fbe973368e8434cdf46dca06f7a25f31d6bc2
Billy <tnehd1998@gmail.com>
2021-12-21 10:55:50 +0900

    리팩터링 2판 (pg126~146)

---
+++ 읽은 책/리팩터링 2판/chapter4/src/test/Province.test.js
@@ -0,0 +1,23 @@
+const { Province, sampleProvinceData } = require("../Province");
+
+describe("province", function () {
+  let asia;
+
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
+  it("shortfall", function () {
+    expect(asia.shortfall).toBe(5);
+  });
+
+  it("profit", function () {
+    expect(asia.profit).toBe(230);
+  });
+
+  it("change production", function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
+});
