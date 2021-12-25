5f555f21addaf8d3d01fe348b5a36351d294305a
Billy <tnehd1998@gmail.com>
2021-12-25 18:35:11 +0900

    TypeScript: The Complete Developer's Guide (Let's Build a Web Framework)

---
+++ Typescript/강의/Udemy/TypeScript - The Complete Developer's Guide/13. Let's Build a Web Framework/web/src/index.ts
@@ -0,0 +1,13 @@
+import { User } from "./models/User";
+import { UserEdit } from "./views/UserEdit";
+
+const user = User.buildUser({ name: "NAME", age: 20 });
+
+const root = document.getElementById("root");
+
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+} else {
+  throw new Error("Root element not found");
+}
