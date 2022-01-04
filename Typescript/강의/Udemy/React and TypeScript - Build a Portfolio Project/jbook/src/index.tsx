a12e488dace200843b91d4e8d7bb667c1affe6a9
Billy <tnehd1998@gmail.com>
2022-01-04 19:48:12 +0900

    React&TypeScript: Build a Portfolio Project (Safely Handling Untrusted Code Execution)

--- Typescript/강의/Udemy/React and TypeScript - Build a Portfolio Project/jbook/src/index.tsx
+++ Typescript/강의/Udemy/React and TypeScript - Build a Portfolio Project/jbook/src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
 
@@ -28,14 +29,14 @@ const App = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         "process.env.NODE_ENV": '"production"',
         global: "window",
       },
     });
 
-    console.log(result);
+    // console.log(result);
 
     setCode(result.outputFiles[0].text);
   };
