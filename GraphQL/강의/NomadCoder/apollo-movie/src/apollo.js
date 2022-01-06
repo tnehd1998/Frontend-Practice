51eaf48db908183da377e5c09dc14f7d17b1a2c1
Billy <tnehd1998@gmail.com>
2022-01-06 21:32:06 +0900

    GraphQL : GraphQL로 영화 웹 앱 만들기

---
+++ GraphQL/강의/NomadCoder/apollo-movie/src/apollo.js
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.modify({
+          id: `Movie:${id}`,
+          fields: {
+            isLiked: (isLiked) => !isLiked,
+          },
+        });
+      },
+    },
+  },
+});
+
+export default client;
---
+++ GraphQL/강의/NomadCoder/apollo-movie/src/components/Movie.jsx
@@ -0,0 +1,41 @@
+import { gql, useMutation } from "@apollo/client";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!, $isLiked: Boolean!) {
+    likeMovie(id: $id, isLiked: $isLiked) @client
+  }
+`;
+
+const Container = styled.div`
+  height: 380px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 7px;
+`;
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Movie = ({ id, medium_cover_image, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id), isLiked },
+  });
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={medium_cover_image} />
+      </Link>
+      <button onClick={likeMovie}>{isLiked ? "Unlike" : "Like"}</button>
+    </Container>
+  );
+};
+
+export default Movie;
