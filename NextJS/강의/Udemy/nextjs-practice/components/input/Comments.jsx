f89f4b8f6c5b3f576f01aa93a8576cffba30bd3d
이수동 <tnehd1998@gmail.com>
2022-03-20 09:33:49 +0900

    NextJS : Working with App-wide State

--- NextJS/강의/Udemy/nextjs-practice/components/input/Comments.jsx
+++ NextJS/강의/Udemy/nextjs-practice/components/input/Comments.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import NotificationContext from "../../store/notification-context";
 import CommentList from "./CommentList";
 import classes from "./Comments.module.css";
 import NewComment from "./NewComment";
@@ -6,14 +7,21 @@ import NewComment from "./NewComment";
 function Comments(props) {
   const { eventId } = props;
 
+  const notificationCtx = useContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComments(true);
       fetch(`/api/comments/${eventId}`)
         .then((response) => response.json())
-        .then((data) => setComments(data.comments));
+        .then((data) => {
+          setComments(data.comments);
+          setIsFetchingComments(false);
+        });
     }
   }, [showComments]);
 
@@ -22,6 +30,12 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: "Sending comment...",
+      message: "Your comment is currently being stored.",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -29,8 +43,29 @@ function Comments(props) {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong");
+        });
+      })
+      .then((data) =>
+        notificationCtx.showNotification({
+          title: "Success",
+          message: "Comment has been added.",
+          status: "success",
+        })
+      )
+      .catch((error) =>
+        notificationCtx.showNotification({
+          title: "Error",
+          message: error.message || "Something went wrong",
+          status: "error",
+        })
+      );
   }
 
   return (
@@ -39,7 +74,8 @@ function Comments(props) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
     </section>
   );
 }
--- NextJS/강의/Udemy/nextjs-practice/components/layout/Layout.jsx
+++ NextJS/강의/Udemy/nextjs-practice/components/layout/Layout.jsx
@@ -1,11 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import NotificationContext from "../../store/notification-context";
+import Notification from "../ui/Notification";
 import MainHeader from "./MainHeader";
 
 const Layout = (props) => {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 };
