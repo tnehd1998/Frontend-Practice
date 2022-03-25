9412d48f0ee66aa964b278c3215a68a4deee0c5d
이수동 <tnehd1998@gmail.com>
2022-03-25 10:37:41 +0900

    NextJS : React Hook Form

---
+++ NextJS/강의/NomadCoder/carrot-market/pages/forms.tsx
@@ -0,0 +1,59 @@
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+  errors?: string;
+}
+
+export default function Forms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<LoginForm>({
+    mode: "onSubmit",
+  });
+
+  const onValid = (data: LoginForm) => {
+    setError("username", { message: "Taken username" });
+  };
+  const onInvalid = (errors: FieldErrors) => {};
+
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "Username should be longer than 5 characters",
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      {errors.username?.message}
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      {errors.email?.message}
+      <input
+        {...register("password", { required: "Password is required" })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
