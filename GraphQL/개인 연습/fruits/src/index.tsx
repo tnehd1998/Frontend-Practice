d02d899023e446dbaf18269e7a9bbaf5183d5c13
이수동 <tnehd1998@gmail.com>
2022-04-30 09:32:10 +0900

    GraphQL: Fruits 초기 세팅

---
+++ GraphQL/개인 연습/fruits/src/index.tsx
@@ -0,0 +1,23 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://fruits-api.netlify.app/graphql",
+});
+
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
+reportWebVitals();
