472ee712f3330499df335a4aa1f1011c9162b945
이수동 <tnehd1998@gmail.com>
2022-04-30 10:51:13 +0900

    GraphQL: Fruits 모든 과일 불러오기

--- GraphQL/개인 연습/fruits/src/pages/Home.tsx
+++ GraphQL/개인 연습/fruits/src/pages/Home.tsx
@@ -1,7 +1,68 @@
+import { useQuery, gql } from "@apollo/client";
 import React from "react";
 
+const GET_FRUITS = gql`
+  query allFruits {
+    fruits {
+      id
+      scientific_name
+      fruit_name
+      description
+      producing_countries {
+        country
+      }
+    }
+  }
+`;
+
+interface IProducingCountries {
+  country: string;
+}
+
+interface IFruit {
+  id: string;
+  scientific_name: string;
+  fruit_name: string;
+  description: string;
+  producing_countries: IProducingCountries[];
+}
+
+interface IFruits {
+  fruits: IFruit[];
+}
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const { data, loading, error } = useQuery<IFruits>(GET_FRUITS);
+
+  if (loading || error) {
+    return <p>{error ? error.message : "Loading..."}</p>;
+  }
+
+  return (
+    <div>
+      {data?.fruits.map((fruit) => (
+        <div
+          key={fruit.id}
+          style={{
+            border: "1px solid black",
+            padding: "40px",
+            margin: "20px",
+            cursor: "pointer",
+          }}
+        >
+          <h1>Fruit Name : {fruit.fruit_name}</h1>
+          <h2>Scientific Name : {fruit.scientific_name}</h2>
+          <p>Description : {fruit.description}</p>
+          <div>
+            <h3>Producing Country</h3>
+            {fruit.producing_countries.map((country) => (
+              <p>{country.country}</p>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
