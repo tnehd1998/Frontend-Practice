1c866b07b3286d75b2fbff5efa48eb35928eb2b5
이수동 <tnehd1998@gmail.com>
2022-06-27 20:40:21 +0900

    :sparkles: Svelte : Conditional Rendering

--- Svelte/svelte-project/src/App.svelte
+++ Svelte/svelte-project/src/App.svelte
@@ -15,6 +15,28 @@
   <button on:click={add5} class="btn btn-primary">Add five</button>
   <p>The number multiplied by 2 is : {multiple}</p>
   <p>The number multiplied by 2 is : {divide}</p>
+  {#if number < 10 }
+    <div class="alert alert-success shadow-lg">
+      <div class="flex-1">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <p>Great Numbers!</p>
+      </div>
+    </div>
+  {:else if 10 < number && number < 30}
+    <div class="alert alert-warning shadow-lg">
+      <div class="flex-1">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <p>The numbers are getting higher!</p>
+      </div>
+    </div>
+  {:else} 
+    <div class="alert alert-error shadow-lg">
+      <div class="flex-1">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <p>Numbers are too high;;;</p>
+      </div>
+    </div>
+  {/if}
 </main>
 
 <style>
