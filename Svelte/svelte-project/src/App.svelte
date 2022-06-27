bad130506a5ccc50e392866d53de47c026b48889
이수동 <tnehd1998@gmail.com>
2022-06-27 20:26:54 +0900

    :sparkles: Svelte : Reactivity

---
+++ Svelte/svelte-project/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let number = 1
+
+  function add5(){
+    number +=5
+  }
+
+  $: multiple = number*2
+
+  $: divide = number/2
+</script>
+
+<main class="flex flex-col justify-center items-center h-screen space-y-4">
+  <p>The number is: {number}</p>
+  <button on:click={add5} class="btn btn-primary">Add five</button>
+  <p>The number multiplied by 2 is : {multiple}</p>
+  <p>The number multiplied by 2 is : {divide}</p>
+</main>
+
+<style>
+ 
+</style>
